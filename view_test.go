a0d5acc3fbb55926136e499c4cdc536a863526b7
abadojack <abadojack@gmail.com>
2016-06-29 03:16:23 +0530

    Remove unused variable in test file.

--- view_test.go
+++ view_test.go
@@ -3,7 +3,7 @@ package gocensys
 import "testing"
 
 func Test_ViewDocument(t *testing.T) {
-	r, err := api.ViewDocument("websites", "google.com")
+	_, err := api.ViewDocument("websites", "google.com")
 	if err != nil {
 		t.Fatal(err)
 	}
